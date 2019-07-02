3c122eff206c22c7824bc04fca2dc0acd61fe427
muhaimin <collabart8@gmail.com>
2019-07-02 14:58:09 +0700

    Berhasil dude

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\Schema;
 class AppServiceProvider extends ServiceProvider
 {
     /**
-     * Register any application services.
+     * Register any application services. 
      *
      * @return void
      */
