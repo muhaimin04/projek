126f19da13a9cc5d72b57927a31bb778df249114
muhaimin <collabart8@gmail.com>
2019-07-05 16:31:42 +0700

    baru index

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,8 @@ Route::get('/home', 'HomeController@index')->name('home');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+
+Route::get('/index', function () {
+    return view('layouts.index');
+});
