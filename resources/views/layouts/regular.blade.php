90fb9407d525a6e2fb3b2dc61be8b2c98246171e
muhaimin <collabart8@gmail.com>
2019-07-06 15:15:53 +0700

    tamplate

--- resources/views/layouts/regular.blade.php
+++ resources/views/layouts/regular.blade.php
@@ -1,164 +1,157 @@
-<!DOCTYPE HTML>
+<!DOCTYPE html>
 <html lang="en">
+
 <head>
-	<title>Quite Light</title>
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-	<meta charset="UTF-8">
-	
-	<!-- Font -->
-	<link href="https://fonts.googleapis.com/css?family=Poppins:400,700" rel="stylesheet">
-	
-	<!-- Stylesheets -->
-	
-	<link href="{{ asset ('assets/frontend/plugin-frameworks/bootstrap.min.css') }}" rel="stylesheet">
-	<link href="{{ asset ('assets/frontend/plugin-frameworks/swiper.css') }}" rel="stylesheet">
-	
-	<link href="{{ asset ('assets/frontend/fonts/ionicons.css') }}" rel="stylesheet">
-	
-		
-	<link href="{{ asset ('assets/frontend/common/styles.css') }}" rel="stylesheet">
-	
-	
+    <meta charset="UTF-8">
+    <meta name="description" content="">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- The above 4 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+
+    <!-- Title  -->
+    <title>World - Blog &amp; Magazine Template</title>
+
+    <!-- Favicon  -->
+    <link rel="icon" href="{{ asset ('assets/frontend/img/core-img/favicon.ico') }}">
+
+    <!-- Style CSS -->
+    <link rel="stylesheet" href="{{ asset ('assets/frontend/style.css') }}">
+
 </head>
+
 <body>
-	
-	<header>
-		<a class="logo" href="#"><img src="images/logo-white.png" alt="Logo"></a>
-		
-		<div class="right-area">
-			<form class="src-form">
-				<button type="submit"><i class="ion-search"></i></a></button>
-				<input type="text" placeholder="Search here">
-			</form><!-- src-form -->
-		</div><!-- right-area -->
-		
-		<a class="menu-nav-icon" data-menu="#main-menu" href="#"><i class="ion-navicon"></i></a>
-		
-		<ul class="main-menu" id="main-menu">
-			<li><a href="index.html">Home</a></li>
-			<li class="drop-down"><a href="#!">Sport<i class="ion-arrow-down-b"></i></a>
-				<ul class="drop-down-menu drop-down-inner">
-					<li><a href="#">PAGE 1</a></li>
-					<li><a href="#">PAGE 2</a></li>
-				</ul>
-			</li>
-			<li><a href="#">Travel</a></li>
-			<li><a href="#">Beauty</a></li>
-			<li><a href="#">Music</a></li>
-			<li><a href="#">Art</a></li>
-			<li><a href="#">Fashion</a></li>
-			<li><a href="#">Contact</a></li>
-		</ul>
-		
-		<div class="clearfix"></div>
-	</header>
-	
-	
-	<div class="slider-main h-500x h-sm-auto pos-relative pt-95 pb-25">
-		<div class="img-bg bg-1 bg-layer-4"></div>
-		<div class="container-fluid h-100 mt-xs-50">
-			<div class="dplay-tbl">
-				<div class="dplay-tbl-cell color-white text-center">
-				
-					<h1 class="ptb-50"><b>Terms of Use</b></h1>
-					
-				</div><!-- dplay-tbl-cell -->
-			</div><!-- dplay-tbl -->
-		</div><!-- container -->
-	</div><!-- slider-main -->
-	
-	
-	<section class="bg-1-white">
-		<div class="container">
-			<div class="row">
-				<div class="col-lg-12">
-					<div class="bg-white p-50">
-						<p>At that event and the ones that followed, plenty of senior citizens — among them librarians, property developers, teachers, doctors — proudly 
-						and happily recounted their acid trips. It was both charming and disconcerting to hear someone my father’s age telling me how they’d tripped their 
-						balls off on the beach in 1968. It shouldn’t have been a surprise, I suppose. Research shows that something like 20 million Americans have used LSD, 
-						many of those back in its 1960s heyday. Still, it was fascinating to hear how their experiences with psychedelics shaped and transformed.</p>
-						
-						<h3 class="mtb-50 mtb-sm-30"><b>“I don’t think I would have attempted such a complicated, multifaceted work had I not been microdosing with LSD.”</b></h3>
-						
-						<p>Though I was eager to share the results of the experiment with others who suffer from depression or other mood issues, my previous book, 
-						Love and Treasure, was a novel set against the backdrop of the Holocaust in Hungary. 
-						In support of that novel, I traveled around the country from Jewish community center to synagogue to Jewish book club.</p>
-						
-					</div><!-- col-sm-3 -->
-				</div><!-- col-sm-3 -->
-			</div><!-- row -->
-		</div><!-- container -->
-	</section>
-	
-	
-	<footer class="bg-191 color-ash pt-50 pb-20 text-left center-sm-text">
-		
-		<div class="container-fluid">
-			<div class="row">
-			
-				<div class="col-lg-1"></div>
-				
-				<div class="col-md-4 col-lg-6 mb-30">
-					<div class="card h-100">
-						<div class="dplay-tbl">
-							<div class="dplay-tbl-cell">
-							
-								<a href="#"><img src="images/logo-white.png"></a>
-								<p class="color-ash mt-25"><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="ion-heart" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
+    <!-- Preloader Start -->
+    <div id="preloader">
+        <div class="preload-content">
+            <div id="world-load"></div>
+        </div>
+    </div>
+    <!-- Preloader End -->
+
+    <!-- ***** Header Area Start ***** -->
+    <header class="header-area">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <nav class="navbar navbar-expand-lg">
+                        <!-- Logo -->
+                        <a class="navbar-brand" href="/"><img src="{{ asset ('assets/frontend/img/core-img/logo.png') }}" alt="Logo"></a>
+                        <!-- Navbar Toggler -->
+                        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#worldNav" aria-controls="worldNav" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                        <!-- Navbar -->
+                        <div class="collapse navbar-collapse" id="worldNav">
+                            <ul class="navbar-nav ml-auto">
+                                <li class="nav-item active">
+                                    <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
+                                </li>
+                                <li class="nav-item dropdown">
+                                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Pages</a>
+                                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                        <a class="dropdown-item" href="/">Home</a>
+                                        <a class="dropdown-item" href="kategori">Catagory</a>
+                                        <a class="dropdown-item" href="single_blog">Single Blog</a>
+                                        <a class="dropdown-item" href="regular">Regular Page</a>
+                                        <a class="dropdown-item" href="contact">Contact</a>
+                                    </div>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">Gadgets</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">Lifestyle</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">Video</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="contact">Contact</a>
+                                </li>
+                            </ul>
+                            <!-- Search Form  -->
+                            <div id="search-wrapper">
+                                <form action="#">
+                                    <input type="text" id="search" placeholder="Search something...">
+                                    <div id="close-icon"></div>
+                                    <input class="d-none" type="submit" value="">
+                                </form>
+                            </div>
+                        </div>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </header>
+    <!-- ***** Header Area End ***** -->
+
+    <!-- ********** Hero Area Start ********** -->
+    <div class="hero-area height-400 bg-img background-overlay" style="background-image: url(assets/frontend/img/blog-img/bg4.jpg);"></div>
+    <!-- ********** Hero Area End ********** -->
+
+    <div class="regular-page-wrap section-padding-100">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-12 col-md-10 col-lg-8">
+                    <div class="page-content">
+                        <h6>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce enim nulla, mollis eu metus in, sagittis fringilla tortor. Phasellus eget purus id felis dignissim convallis. Suspendisse et augue dui. Morbi gravida sed justo vel venenatis. Ut tempor pretium maximus. Donec libero diam, faucibus vitae lectus nec, accumsan gravida dui. Nam interdum mi eget lacus aliquet, sit amet ultricies magna pharetra. In ut odio a ligula egestas pretium et quis sapien. Etiam faucibus magna eu porta vulputate. Aliquam euismod rhoncus malesuada. Nunc rutrum hendrerit semper.</h6>
+                        <h6>Maecenas vitae sem varius, imperdiet nisi a, tristique nisi. Sed scelerisque suscipit leo cursus accumsan. Donec vel turpis quam. Mauris non nisl nec nunc gravida ullamcorper id vestibulum magna. Donec non velit finibus, laoreet arcu nec, facilisis augue. Aliquam sed purus id erat accumsan congue. Mauris semper ullamcorper nibh non pellentesque. Aenean euismod purus sit amet quam vehicula ornare.</h6>
+                        <h6>Donec orci dolor, pretium in luctus id, consequat vitae nibh. Quisque hendrerit, lorem sit amet mollis malesuada, urna orci volutpat ex, sed scelerisque nunc velit et massa. Sed maximus id erat vel feugiat. Phasellus bibendum nisi non urna bibendum elementum. Aenean tincidunt nibh vitae ex facilisis ultrices. Integer ornare efficitur ultrices. Integer neque lectus, venenatis at pulvinar quis, aliquet id neque. Mauris ultrices consequat velit, sed dignissim elit posuere in. Cras vitae dictum dui.</h6>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- ***** Footer Area Start ***** -->
+    <footer class="footer-area">
+        <div class="container">
+            <div class="row">
+                <div class="col-12 col-md-4">
+                    <div class="footer-single-widget">
+                        <a href="#"><img src="{{ asset ('assets/frontend/img/core-img/logo.png') }}" alt=""></a>
+                        <div class="copywrite-text mt-30">
+                            <p><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
 <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
-								
-							</div><!-- dplay-tbl-cell -->
-						</div><!-- dplay-tbl -->
-					</div><!-- card -->
-				</div><!-- col-lg-4 col-md-6 -->
-				
-				<div class="col-md-4 col-lg-2 mb-30">
-					<div class="card h-100">
-						<div class="dplay-tbl">
-							<div class="dplay-tbl-cell">
-							
-								<ul class="list-a-plr-10">
-									<li><a href="#"><i class="ion-social-facebook"></i></a></li>
-									<li><a href="#"><i class="ion-social-twitter"></i></a></li>
-									<li><a href="#"><i class="ion-social-youtube"></i></a></li>
-								</ul>
-								
-							</div><!-- dplay-tbl-cell -->
-						</div><!-- dplay-tbl -->
-					</div><!-- card -->
-				</div><!-- col-lg-4 col-md-6 -->
-				
-				<div class="col-md-4 col-lg-2 mb-30 text-left">
-					<div class="card h-100">
-						<div class="dplay-tbl">
-							<div class="dplay-tbl-cell">
-								<form class="form-block form-brdr-b mx-w-400x m-auto">
-						
-									<input class="color-white ptb-15 center-sm-text" type="text" placeholder=" Your Email">
-									<button class="mt-30 btn-brdr-grey color-ash w-100 text-center" type="submit">SUBSCRIBE</button>
-								
-								</form>
-							</div><!-- dplay-tbl-cell -->
-						</div><!-- dplay-tbl -->
-					</div><!-- card -->
-				</div><!-- col-lg-4 col-md-6 -->
-				
-			</div><!-- row -->
-		</div><!-- container -->
-	</footer>
-	
-	<!-- SCIPTS -->
-	
-	<script src="{{ asset ('assets/frontend/plugin-frameworks/jquery-3.2.1.min.js') }}"></script>
-	
-	<script src="{{ asset ('assets/frontend/plugin-frameworks/bootstrap.min.js') }}"></script>
-	
-	<script src="{{ asset ('assets/frontend/plugin-frameworks/swiper.js') }}"></script>
-	
-	
-	<script src="{{ asset ('assets/frontend/common/scripts.js') }}"></script>
-	
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 col-md-4">
+                    <div class="footer-single-widget">
+                        <ul class="footer-menu d-flex justify-content-between">
+                            <li><a href="#">Home</a></li>
+                            <li><a href="#">Fashion</a></li>
+                            <li><a href="#">Lifestyle</a></li>
+                            <li><a href="#">Contact</a></li>
+                            <li><a href="#">Gadgets</a></li>
+                            <li><a href="#">Video</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-12 col-md-4">
+                    <div class="footer-single-widget">
+                        <h5>Subscribe</h5>
+                        <form action="#" method="post">
+                            <input type="email" name="email" id="email" placeholder="Enter your mail">
+                            <button type="button"><i class="fa fa-arrow-right"></i></button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <!-- ***** Footer Area End ***** -->
+
+    <!-- jQuery (Necessary for All JavaScript Plugins) -->
+    <script src="{{ asset ('assets/frontend/js/jquery/jquery-2.2.4.min.js') }}"></script>
+    <!-- Popper js -->
+    <script src="{{ asset ('assets/frontend/js/popper.min.js') }}"></script>
+    <!-- Bootstrap js -->
+    <script src="{{ asset ('assets/frontend/js/bootstrap.min.js') }}"></script>
+    <!-- Plugins js -->
+    <script src="{{ asset ('assets/frontend/js/plugins.js') }}"></script>
+    <!-- Active js -->
+    <script src="{{ asset ('assets/frontend/js/active.js') }}"></script>
+
 </body>
+
 </html>
