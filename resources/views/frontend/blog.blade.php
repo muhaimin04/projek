ba759531544f37b6e15d8bb03ceaecbae44a67b8
muhaimin <collabart8@gmail.com>
2019-08-26 19:53:39 +0700

    tampe

--- resources/views/frontend/blog.blade.php
+++ resources/views/frontend/blog.blade.php
@@ -42,7 +42,7 @@
 		<div class="ln-title">Latest News</div>
 		<div class="news-ticker">
 			<div class="news-ticker-contant">
-				<div class="nt-item"><span class="new">new</span>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </div>
+				<div class="nt-item"><span class="new">new</span>Lorem ipsum dolor sit amet, consectetur adipiscing el. </div>
 				<div class="nt-item"><span class="strategy">strategy</span>Isum dolor sit amet, consectetur adipiscing elit. </div>
 				<div class="nt-item"><span class="racing">racing</span>Isum dolor sit amet, consectetur adipiscing elit. </div>
 			</div>
